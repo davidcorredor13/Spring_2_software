f2551c15a89f5dfd14d87ddc2543b85c74f11e37
nelsongil30 <94405974+nelsongil30@users.noreply.github.com>
2021-11-15 16:35:51 -0500

    Update MascotaRepo.java

--- peristencia/src/principal/Java/co/edu/uniquindio/proyecto/repositorios/MascotaRepo.java
+++ peristencia/src/principal/Java/co/edu/uniquindio/proyecto/repositorios/MascotaRepo.java
@@ -6,6 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
+/*
+se crea el repositorio */
+
 @Repository
 // Clase para la interfaz de la mascota
 public interface MascotaRepo extends JpaRepository <Mascota,Integer>{
