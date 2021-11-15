98ec6c0da2468c9b8f21f8be8bcf1459d61aa1d9
AlbaMarin94 <93091253+AlbaMarin94@users.noreply.github.com>
2021-11-15 16:39:22 -0500

    adicion de comentarios a a interfaz PlanServicio

--- negocio/src/main/java/co/edu/uniquindio/proyecto/servicios/PlanServicio.java
+++ negocio/src/main/java/co/edu/uniquindio/proyecto/servicios/PlanServicio.java
@@ -4,23 +4,36 @@ import co.edu.uniquindio.proyecto.entidades.Categoria;
 import co.edu.uniquindio.proyecto.entidades.Plan;
 import co.edu.uniquindio.proyecto.entidades.Usuario;
 
-
 import java.util.List;
 
+/**
+ * @Description clase que determina la interfaz de un paln servicio
+ */
 public interface PlanServicio {
 
-
-
+    /**
+     * metodo que permite registrar un plan
+     */
     Plan registrarPlan(Plan p) throws  Exception;
 
+    /**
+     * metodo que permite actualizar un plan
+     */
     void actualizarPlan(Plan p) throws  Exception;
 
+    /**
+     * Metodo que permite eliminar un plan
+     */
     void eliminarPlan(Integer codigo) throws  Exception;
 
+    /**
+     * metodo que permite obtener un plan
+     */ 
     Plan obtenerPlan(Integer codigo) throws Exception;
-;
 
+    /**
+     * metodo que permite buscar planes por filtros
+     */
     List<Plan> buscarPlan(String nombreProducto, String[] filtros);
-
 
 }
