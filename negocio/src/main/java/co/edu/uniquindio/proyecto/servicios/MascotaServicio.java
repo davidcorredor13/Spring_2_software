75db51223a97ed7c0c1b58c3ae5384c376f001a3
AlbaMarin94 <93091253+AlbaMarin94@users.noreply.github.com>
2021-11-15 16:31:42 -0500

    adicion comentarios a la interfaz MascotaServicio

--- negocio/src/main/java/co/edu/uniquindio/proyecto/servicios/MascotaServicio.java
+++ negocio/src/main/java/co/edu/uniquindio/proyecto/servicios/MascotaServicio.java
@@ -3,6 +3,9 @@ package co.edu.uniquindio.proyecto.servicios;
 import co.edu.uniquindio.proyecto.entidades.Mascota;
 import co.edu.uniquindio.proyecto.entidades.Usuario;
 
+/**
+ * @Description calse que determina la interfaz mascota servicio
+ */
 public interface MascotaServicio {
 
     Mascota obtenerMascota(Integer codigo) throws Exception;
