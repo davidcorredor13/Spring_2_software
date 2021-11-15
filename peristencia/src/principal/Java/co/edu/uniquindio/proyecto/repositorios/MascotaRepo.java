07e87e5f25c8fbb567c9e23536de8247cf531ca2
davidcorredor13 <64997596+davidcorredor13@users.noreply.github.com>
2021-11-15 16:22:26 -0500

    Update and rename peristencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/MascotaRepo.java to peristencia/src/principal/Java/co/edu/uniquindio/proyecto/repositorios/MascotaRepo.java
    Se agrega comentarios

--- peristencia/src/principal/Java/co/edu/uniquindio/proyecto/repositorios/MascotaRepo.java
+++ peristencia/src/principal/Java/co/edu/uniquindio/proyecto/repositorios/MascotaRepo.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 @Repository
+// Clase para la interfaz de la mascota
 public interface MascotaRepo extends JpaRepository <Mascota,Integer>{
 
 
